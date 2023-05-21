5c1f96b630107bb800b12e22e9787f2d313ae7f6
Taechit Phowthongbutr <pun.152@gmail.com>
2023-05-21 16:07:42 +0700

    fix kneebot

--- kneeBottom/__init__.py
+++ kneeBottom/__init__.py
@@ -1,10 +1,14 @@
+import numpy as np
+
 def check_row(row):
+    row = np.append(row, 0, axis=None)
+    row = np.insert(row, 0,0)
     index = []
     for j in range(1,row.shape[0]):#col
         if row[j] > row[j-1] :
-            index.append(j) #ขึ้น
+            index.append(j-1) #ขึ้น
         elif row[j] < row[j-1] :
-            index.append(j-1) #ลง
+            index.append(j-2) #ลง
     return index # เก็บตำแหน่งขึนลง
 
 def check_peak(mask,x,y):
@@ -23,6 +27,10 @@ def check_peak(mask,x,y):
         return x,y+y1
 
 def find_tibia_point(mask):
+  tmp = np.sum(mask, axis=1)
+  row_where = np.where(tmp > 0)[0]
+  lowest = row_where[-1]
+  highest = row_where[0]  
   col_idx = 0
   row_idx = 0
   x = 0
@@ -31,22 +39,25 @@ def find_tibia_point(mask):
   peaks = []
   #find W,H
   W = 0
-  max_row = 0
-  for i in range(mask.shape[0]):
+  max_row_l = 0
+  max_row_r = 0
+  for i in range(highest,lowest):
     row = mask[i]
     index = check_row(row)
     if len(index) == 2:
-      if index[1]-index[0] > max_row:
-        max_row = index[1]-index[0]  
+      if index[1]-index[0] > max_row_r-max_row_l:
+        max_row_r = index[1]
+        max_row_l = index[0]  
         W = i
   ###
-  for i in range(mask.shape[0]):
-    row = mask[i]
+  mid_of_max = (max_row_r + max_row_l)//2
+  Q1 = (max_row_r-max_row_l)//4
+  for i in range(highest,lowest):
+    row = mask[i, mid_of_max-Q1:mid_of_max+Q1] #แต่ละแถว
+    tab = mid_of_max-Q1
     index = check_row(row)
-    if len(index) != 0: # เจอเข่า
-      mid = (index[0]+index[1])//2
-      Q1 = max_row//4
-      H = W-i
-      x,y = check_peak(mask[i:i+H,mid-Q1:mid+Q1],i,mid-Q1)
+    if len(index) != 0 and len(index)%2 == 0: # เจอเข่า
+      mid = (index[0]+tab+index[1]+tab)//2
+      x,y = check_peak(mask[i:W,mid-Q1:mid+Q1],i,mid-Q1)
       break
   return x,y
